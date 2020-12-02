1a5f9b1d132ea8eda87fcbb44ee9c1cbc55d168d
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-12-02 15:48:43 -0300

    finally clustal results are formatted properly

--- TPFinal/backend/api - Copy.py
+++ TPFinal/backend/api - Copy.py
@@ -115,40 +115,41 @@ def getInfo():
         file.writelines("\n")
         file.close() 
 
-    #s = "blastp -query ./backend/db/1.fasta -out " + blast_path + " -db " + db_path + " -evalue " + str(e_value) + " -outfmt 5"
     s = "blastp -query " + base_fasta_file + " -out " + blast_path + " -db " + db_path + " -evalue " + str(e_value) + " -outfmt 5"
     os.system(s)
 
     blast_records = NCBIXML.parse(open(blast_path))
-
     #abro el archivo donde voy a guardar el fasta post blast con las secuencias homólogas
     file = open(base_fasta_file, "w") 
 
     #recorre el archivo devuelto y obtengo las homólogas que cumplen la identidad pasada por argumento correspondiente al campo identity
-    for blast_record in blast_records:
+    for blast_record in blast_records:          
         sorted_aligntments = []
         for alignment in blast_record.alignments:
+            title = str(alignment.title)
+            title = title[4]+title[5]+title[6]+title[7]
             identities = alignment.hsps[0].identities
             align_length = alignment.hsps[0].align_length
             porcent =  identities / align_length * 100
-
             if (porcent > identity):
-                sorted_aligntments.append((alignment.hsps[0].query, porcent))
+                sorted_aligntments.append((title, alignment.hsps[0].query, porcent))
         sorted_aligntments.sort(key=lambda x: x[1])
-        reversed_alignments = sorted_aligntments[::-1]
+        reversed_alignments = sorted_aligntments[::-1]  
 
     #abro el archivo donde voy a guardar las n secuencias correspondiente al argumento pasado en el json del campo num_align
     # en el fasta para luego ser tomado por clustal
     id = 0
+    """cada elemento de la lista reversed_alignments son tripletas 
+        cada uno compuesto por la sig estructura (id, seq, porcentaje_identidad)
+        por eso el [0] me devuelve el id, el [1] la seq y el [2] no lo uso, no es necesario para el fasta
+    """
     while id < num_align:
-        file.writelines("> " + str(id)) 
+        file.writelines("> " + str(reversed_alignments[id][0]) ) 
         file.writelines("\n")
-        file.writelines(str(reversed_alignments[id][0])) 
+        file.writelines(str(reversed_alignments[id][1])) 
         file.writelines("\n")
         id = id + 1  
-
     file.close() 
-
     
     clustal_output_path = os.path.join(owd, "backend")
     clustal_output_path = os.path.join(clustal_output_path, "clustal")
@@ -156,19 +157,16 @@ def getInfo():
         os.mkdir(clustal_output_path)
     clustal_output_path = os.path.join(clustal_output_path, "out_clustal_file.fa")
     
-    clustalw_cline = ClustalwCommandline(cmd = clustalw_exe, infile=base_fasta_file, outfile= clustal_output_path, output="fasta")
+    clustalw_cline = ClustalwCommandline(cmd = clustalw_exe, infile=base_fasta_file, outfile= clustal_output_path, output="clustal")
     clustalw_cline() 
 
-    with open(clustal_output_path, "r") as a_file:
-        seq = ""
-        lines = list(a_file)[::-1]
-        for line in lines:
-            if ">" in line:
-                seq = ""            
-            else:
-                seq = line.rstrip('\n') + seq
-            print(seq)
-          
+    records = SeqIO.parse(clustal_output_path, "clustal")
+    seqs= []
+    for record in records:
+        seq = (str(record.description), str(record.seq))
+        seqs.append(seq)
+    print(str(seqs))
+
     result["id"]=id
     json_object = json.dumps(result, indent = 4)
     return (json_object)
